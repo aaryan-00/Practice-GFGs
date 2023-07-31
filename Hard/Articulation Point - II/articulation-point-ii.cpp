935e01b14b75b0bfbeaea516d5e19037d8dcb427
Aaryan Raaz <120718690+aaryan-00@users.noreply.github.com>
2023-07-31 18:58:31 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Articulation Point - II/articulation-point-ii.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<bool> vis,markArti;
+    int timer=0;
+    void dfs(int node,int par,vector<int>&timeIn,vector<int>&timeLow,vector<int> adj[])
+    {
+        vis[node]=true;
+        timeIn[node]=timeLow[node]=timer;
+        timer++;
+        int child=0;
+        for(auto it:adj[node])
+        {
+            if(it==par) continue;
+            if(vis[it]==false)
+            {
+                dfs(it,node,timeIn,timeLow,adj);
+                timeLow[node]=min(timeLow[node],timeLow[it]);
+                if(timeLow[it]>=timeIn[node] && par!=-1)
+                {
+                    markArti[node]=true;
+                }
+                child++;
+            }
+            else
+            {
+                timeLow[node]=min(timeLow[node],timeIn[it]);
+            }
+        }
+        if(child >1  && par==-1)
+        {
+            markArti[node]=true;
+        }
+    }
+    vector<int> articulationPoints(int n, vector<int>adj[]) {
+        vector<int> timeIn(n),timeLow(n);
+        vis.resize(n,false);
+        markArti.resize(n,false);
+        for(int i=0;i<n;i++)
+        {
+            if(vis[i]==false)
+            {
+                dfs(i,-1,timeIn,timeLow,adj);
+            }
+        }
+        vector<int> ans;
+        for(int i=0;i<n;i++)
+        {
+            if(markArti[i])
+            {
+                ans.push_back(i);
+            }
+        }
+        if(ans.size()==0) return {-1};
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		vector<int> ans = obj.articulationPoints(V, adj);
+		for(auto i: ans)cout << i << " ";
+		cout << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
